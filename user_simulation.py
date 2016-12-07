7d5d31b56a68b888ce5b9a539c77c26b48aa0b5a
Brian Elliott <ellio2bj@dukes.jmu.edu>
2016-12-06 16:44:00 -0800

    IP address update
    Simulating more than one IP attacking the system is now possible.  IPs must fall within the private network though.

--- user_simulation.py
+++ user_simulation.py
@@ -30,12 +30,18 @@ else:
         for line in usrfile:
             users.append(line.strip())
 
-protocol = raw_input('Which protocol do you wish to test? ')
-ip_addr = raw_input('What is the host IP you wich to test against?')
+protocol = raw_input('Which protocol do you wish to test? Protocol:')
+ip_addr = raw_input('What is the host IP you wich to test against? IP:')
+host_ip = raw_input('What are the first three numbers of the IP you wish to attack with? (ex: 192.168.1., Assumes /24 subnet) IP:')
 
+# <== Simulating the multi-user attack ==>
 print 'Simulating user logins with Hydra...'
 while cur_time < target_time:
+    attack_ip = host_ip + str(random.randint(50,80))
+    subprocess.call(['ifconfig','eth0','down'])
+    subprocess.call(['ifconfig','eth0',attack_ip,'netmask','255.255.255.0'])
+    subprocess.call(['ifconfig','eth0','up'])
     subprocess.call(['hydra','-l',random.choice(users),'-p',random.choice(passwords),ip_addr,protocol])
 #    print random.choice(users) 
     cur_time = time.time()
-    
+#    print attack_ip
